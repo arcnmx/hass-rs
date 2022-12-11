7bf2fd8efb187db591c54d73b6ebf9718a5c27c1
arcnmx <git@git.arcn.mx>
2022-12-10 16:36:37 -0800

    fix: users need more public types!

--- crates/mqtt-client/src/lib.rs
+++ crates/mqtt-client/src/lib.rs
@@ -1,13 +1,13 @@
 #![cfg_attr(provide_any, feature(provide_any))]
 #![cfg_attr(provide_any, feature(error_generic_member_access))]
 
-mod availability;
-mod client;
-mod entity;
-mod options;
-mod router;
-mod topics;
-mod tracking;
+pub mod availability;
+pub mod client;
+pub mod entity;
+pub mod options;
+pub mod router;
+pub mod topics;
+pub mod tracking;
 
 pub mod error;
 pub mod mqtt;
@@ -16,3 +16,4 @@ pub use client::{HassMqttClient, Message};
 pub use hass_mqtt_provider::QosLevel;
 pub use hass_mqtt_types as types;
 pub use options::{HassMqttOptions, MqttOptionsError, MqttPersistenceError};
+pub use entity::{EntityTopic, StateTopic, CommandTopic};
